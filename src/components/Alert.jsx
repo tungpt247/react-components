9c1508bd5007caddb9dd8443887f3fbaf32df0d2
Tung Phan <tungpt247@gmail.com>
2016-03-30 17:52:36 +0700

    Add Alert component

---
+++ src/components/Alert.jsx
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react'
+import classNames from 'classnames'
+
+const ALERT_TYPES = [
+  'danger',
+  'error',
+  'info',
+  'primary',
+  'success',
+  'warning'
+  ]
+
+class Alert extends Component {
+  static PropTypes = {
+    type: PropTypes.oneOf(ALERT_TYPES).isRequired
+  }
+
+  render() {
+    var componentClass = classNames('Alert', `Alert--${this.props.type}`, this.props.className)
+
+    return (<div className={componentClass}>{this.props.children}</div>)
+  }
+}
+
+export default Alert
